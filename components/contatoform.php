106a9b62b4f1ebfe6e9f04441108a433754a4ef8
RYuTo94 <ryutoguti@hotmail.com>
2019-06-09 06:34:51 -0300

    Ajustando form contato

--- components/contatoform.php
+++ components/contatoform.php
@@ -7,32 +7,36 @@
   - submit button
 -->
 <!-- FIXME: Arrumar classes para CSS-->
-<div class="container login-container">
+<div class="container">
   <div class="row justify-content-center">
-    <div class="col-10 col-md-6 login-form">
-      <div class='login-form-title'>CONTATE-NOS</div>
-      <form>
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Nome">
+    <div class="col-10 col-md-5">
+      <fieldset>
+        <div class="text-center mt-5">
+        <legend>CONTATE-NOS</legend>
         </div>
-        <div class="form-group">
-          <input type="email" class="form-control" placeholder="Seu email">
-        </div>
-        <div class="form-group">
-          <textarea name="comentario" class ="form-control" cols="8" rows="10" placeholder="Comentários e sugestões"></textarea>
-        </div>
-        <div class="form-row">
-          <div class="col-7 col-md-9">
-            <div class="form-group form-check">
-              <input type="checkbox" class="form-check-input" id="exampleCheck1">
-              <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        <form>
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Nome">
+          </div>
+          <div class="form-group">
+            <input type="email" class="form-control" placeholder="Seu email">
+          </div>
+          <div class="form-group">
+            <textarea name="comentario" class ="form-control" cols="8" rows="10" placeholder="Comentários e sugestões"></textarea>
+          </div>
+          <div class="form-group">
+            <div class="justify-content-between">
+              <div class="form-inline">
+                <div class="form-check ml-0">
+                  <input type="checkbox" class="form-check-input" id="postarNome">
+                  <label class="form-check-label" for="postarNome">Não postar meu nome</label>
+                </div>
+                  <button type="submit" class="btn btn-primary">Submit</button> 
+              </div>
             </div>
           </div>
-          <div class="col-3">
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </div>
-        </div>
-      </form>
+        </form>  
+      </fieldset>
     </div>
   </div>
 </div>
