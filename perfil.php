86ebeb8576f1d1582d30eda6adca185ce67a7502
Bruno Galvao <brunoqgalvao@hotmail.com>
2019-06-28 18:44:19 -0300

    commitando mudanças externas sory galera

--- perfil.php
+++ perfil.php
@@ -1,16 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://kit.fontawesome.com/ba148e3b06.js"></script>
-    <link rel="stylesheet" href="css/perfil.css">
-    <title>Projeto FII</title>
+  <?php include "components/head.php" ?>
   </head>
   <body>
     <?php include "components/header-logon.php" ?>
-    <?php include "components/perfilSection.php" ?>
+    <?php include "components/perfilHead.php" ?>
   </body>
   <!-- DEV: Só desenvolvimento -->
   <script>
