3ee77d83b2585de8b29a06c7840b1a0bf62ccb4c
Bruno Galvao <brunoqgalvao@hotmail.com>
2019-07-01 13:57:49 -0300

    post v2

--- components/postBox.php
+++ components/postBox.php
@@ -25,7 +25,7 @@
   }
 
   .photo-profile {
-    border:none;
+    border: none;
   }
 
   .anchor-username h4 {
@@ -42,7 +42,7 @@
   }
 </style>
 
-<div class="card p-3">
+<div class="card p-3 mb-3">
   <div class="panel-body">
     <section class="post-heading">
       <div class="row">
@@ -50,57 +50,52 @@
           <div class="media">
             <div class="media-left">
               <a href="#">
-                <img class="media-object photo-profile"
-                  src="./assets/itau.jpg" width="40"
-                  height="40" alt="...">
+                <img class="media-object photo-profile" src="./assets/itau.jpg" width="40" height="40" alt="...">
               </a>
             </div>
-            <div class="media-body ml-2 align-items-end">
+            <div class="media-body ml-2 mt-2 align-items-end">
               <a href="#" class="anchor-username">
-                <h4 class="media-heading">Bayu Darmantra</h4>
+                <h4 class="media-heading">Itaú</h4>
               </a>
             </div>
           </div>
         </div>
-        <div class="col-md-1">
-          <a href="#"><i class="glyphicon glyphicon-chevron-down"></i></a>
-        </div>
       </div>
     </section>
-    <section class="post-body">
+    <section class="post-body py-2 bg-container">
+      <div class='bg-container' style="height:200px">
+        <div class='bg' style="background-image:url('assets/itau_banner.jpg')"></div>
+      </div>
+      <div class='post-text pt-2'>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
         turpis sem, dictum id bibendum eget, malesuada ut massa. Ut scel
         erisque nulla sed luctus dapibus. Nulla sit amet mi vitae purus sol
         licitudin venenatis. Praesent et sem urna. Integer vitae lectus nis
-        l. Fusce sapien ante, tristique efficitur lorem et, laoreet ornare lib
-        ero. Nam fringilla leo orci. Vivamus semper quam nunc, sed ornare magna dignissim sed. Etiam interdum justo quis
-        risus
-        efficitur dictum. Nunc ut pulvinar quam. N
-        unc mollis, est a dapibus dignissim, eros elit tempor diam, eu tempus quam felis eu velit.</p>
+        l.</p>
+      </div>
     </section>
     <section class="post-footer">
       <hr>
       <div class="post-footer-option container">
-        <ul class="list-unstyled">
-          <li><a href="#"><i class="glyphicon glyphicon-thumbs-up"></i> Like</a></li>
-          <li><a href="#"><i class="glyphicon glyphicon-comment"></i> Comment</a></li>
-          <li><a href="#"><i class="glyphicon glyphicon-share-alt"></i> Share</a></li>
+        <ul class="list-unstyled d-flex justify-content-center">
+          <li><a href="#">
+              <i class="fa fa-thumbs-up" style="color:var(--verde)"></i>
+              <span class='d-none d-md-inline' style='font-size:0.6em'>Curtir</span>
+            </a></li>
+          <li><a href="#">
+            <i class="fa fa-comment"  style="color:var(--verde)"></i> 
+            <span class='d-none d-md-inline ' style='font-size:0.6em'>Comentar</span>
+          </a></li>
+          <li><a href="#">
+            <i class="fa fa-share"  style="color:var(--verde)"></i> 
+            <span class='d-none d-md-inline' style='font-size:0.6em'>Compartilhar
+              </span
+            </a></li>
         </ul>
       </div>
       <div class="post-footer-comment-wrapper">
         <div class="comment-form">
 
-        </div>
-        <div class="comment">
-          <div class="media">
-            <div class="media-left">
-              <a href="#">
-                <img class="media-object photo-profile"
-                  src="http://0.gravatar.com/avatar/38d618563e55e6082adf4c8f8c13f3e4?s=40&d=mm&r=g" width="32"
-                  height="32" alt="...">
-              </a>
-            </div>
-          </div>
         </div>
       </div>
     </section>
